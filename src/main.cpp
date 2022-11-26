f2595965e85ac8223fd82dfbf7b8ea5d42041136
1Hibiki1 <devang2002@hotmail.co.uk>
2022-11-26 12:36:20 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,9 @@
-#include <cpu/port.h>
-#include <cpu/tm4c.h>
-#include <cpu/interrupts.h>
-#include <libk/mem.h>
-#include <libk/array.h>
-#include <device/serial.h>
-#include <cpu/timer.h>
-#include <libk/stdio.h>
 #include <kernel/kernel.h>
-#include <kernel/process.h>
-#include <libk/arrayqueue.h>
-#include <device/request.h>
-#include <cstdint>
-#include <device/ramdisk.h>
-#include <device/sdcard.h>
-#include <libk/vector.h>
-#include <fs/file.h>
-#include <fs/directory.h>
-#include <fs/fs.h>
-#include <fs/stfs.h>
-#include <device/keyboard.h>
-#include <libk/string.h>
-#include <device/drive.h>
-#include <kernel/syscall.h>
-#include <device/esp01.h>
-
-#include <user/locks-vm/vm.h>
-#include <user/stdio.h>
 #include <user/process/init.h>
-
 #include "kernel/initial_disk.h"
 
 
 const bool format_sd = true;
-
 
 extern "C" void kmain(){
     kernel::init();
